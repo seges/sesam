28cf23c257375b25797022309ac15bab8ddadb58
Juraj Smolinský <juraj.smolinsky@seges.sk>
2014-03-04 14:09:50 +0100

    - change visibility of Disjunction constructor from protected to public

--- sesam-core/src/main/java/sk/seges/sesam/dao/Disjunction.java
+++ sesam-core/src/main/java/sk/seges/sesam/dao/Disjunction.java
@@ -9,7 +9,7 @@ package sk.seges.sesam.dao;
 public class Disjunction extends Junction {
 	private static final long serialVersionUID = -6937397351955413400L;
 	
-	protected Disjunction() {	}
+	public Disjunction() {	}
 	
 	public Junction or(Criterion criterion) {
 		return add(criterion);
