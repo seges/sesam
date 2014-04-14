78e1000d62e834bab417d99377cd40a8a713e358
Peter Sloboda <sloboda@seges.sk>
2014-04-14 12:24:09 +0200

    Correct clone method printer

--- sesam-annotation-processors/sesam-model-domain-processor/src/main/java/sk/seges/sesam/pap/model/printer/clone/ClonePrinter.java
+++ sesam-annotation-processors/sesam-model-domain-processor/src/main/java/sk/seges/sesam/pap/model/printer/clone/ClonePrinter.java
@@ -1,29 +1,32 @@
 package sk.seges.sesam.pap.model.printer.clone;
 
+import java.util.List;
+import java.util.Set;
+
+import javax.tools.Diagnostic;
+
 import sk.seges.sesam.core.pap.model.mutable.api.MutableDeclaredType;
 import sk.seges.sesam.core.pap.model.mutable.api.MutableTypeMirror;
 import sk.seges.sesam.core.pap.model.mutable.api.MutableTypeVariable;
-import sk.seges.sesam.core.pap.model.mutable.utils.MutableProcessingEnvironment;
 import sk.seges.sesam.core.pap.utils.MethodHelper;
 import sk.seges.sesam.core.pap.writer.FormattedPrintWriter;
 import sk.seges.sesam.pap.model.accessor.GenerateCloneAccessor;
 import sk.seges.sesam.pap.model.context.api.TransferObjectContext;
 import sk.seges.sesam.pap.model.model.ConfigurationTypeElement;
+import sk.seges.sesam.pap.model.model.TransferObjectProcessingEnvironment;
+import sk.seges.sesam.pap.model.model.api.domain.DomainType;
 import sk.seges.sesam.pap.model.model.api.dto.DtoDeclaredType;
 import sk.seges.sesam.pap.model.printer.AbstractElementPrinter;
 
-import javax.tools.Diagnostic;
-import java.util.Set;
-
 public class ClonePrinter extends AbstractElementPrinter {
 
-	private final MutableProcessingEnvironment processingEnv;
+	private final TransferObjectProcessingEnvironment processingEnv;
 
 	private static final String RESULT_NAME = "result";
 
 	private boolean active = true;
 
-	public ClonePrinter(MutableProcessingEnvironment processingEnv, FormattedPrintWriter pw) {
+	public ClonePrinter(TransferObjectProcessingEnvironment processingEnv, FormattedPrintWriter pw) {
 		super(pw);
 		this.processingEnv = processingEnv;
 	}
@@ -40,7 +43,7 @@ public class ClonePrinter extends AbstractElementPrinter {
 		DtoDeclaredType dto = configurationTypeElement.getDto();
 
 		pw.println("public ", dto, " clone() {");
-		pw.println(dto, " " + RESULT_NAME + " = new " + dto + "();");
+		pw.println(dto, " " + RESULT_NAME + " = new ", dto, "();");
 		pw.println();
 	}
 
@@ -60,16 +63,31 @@ public class ClonePrinter extends AbstractElementPrinter {
 				MutableDeclaredType iterableType = processingEnv.getTypeUtils().toMutableType(Iterable.class);
 
 				if (processingEnv.getTypeUtils().implementsType(context.getDtoFieldType(), iterableType)) {
-					String newFieldName = "new" + MethodHelper.toMethod(context.getDomainFieldName());
-					pw.println(context.getDtoFieldType() + newFieldName + " = null;" );
+					String newFieldName = "new" + MethodHelper.toMethod(context.getDomainFieldPath());
+					pw.println(context.getDtoFieldType(), " " + newFieldName + " = null;" );
 					pw.println("if (" + MethodHelper.toGetter(context.getDtoFieldName()) + " != null) {");
 					//TODO sometimes it could be interface which is not instantiable, so we have to use
 					//default implementation
-					pw.println(newFieldName + " = new " + context.getDtoFieldType() + "();");
+					pw.println(newFieldName + " = new ",  context.getDtoFieldType(), "();");
 
 					MutableTypeMirror elementType = processingEnv.getTypeUtils().toMutableType(Object.class);
 
-					if (context.getDtoFieldType().getKind().equals(MutableTypeMirror.MutableTypeKind.TYPEVAR)) {
+					if (context.getDtoFieldType().getKind().equals(MutableTypeMirror.MutableTypeKind.CLASS) ||
+						context.getDtoFieldType().getKind().equals(MutableTypeMirror.MutableTypeKind.INTERFACE)) {
+						
+						DtoDeclaredType declaredDto = (DtoDeclaredType)context.getDtoFieldType();
+						if (declaredDto.hasTypeParameters()) {
+							List<? extends MutableTypeVariable> typeVariables = declaredDto.getTypeVariables();
+							
+							if (typeVariables.size() > 1) {
+								processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING, "[WARNING] More type variables are defined in iterable implementation " +
+										context.getDtoFieldType() + ". Using first one!", context.getConfigurationTypeElement().asConfigurationElement());
+
+							}
+							
+							elementType = typeVariables.get(0);
+						}
+					}else if (context.getDtoFieldType().getKind().equals(MutableTypeMirror.MutableTypeKind.TYPEVAR)) {
 						Set<? extends MutableTypeMirror> lowerBounds = ((MutableTypeVariable) context.getDtoFieldType()).getLowerBounds();
 
 						if (lowerBounds != null && lowerBounds.size() > 0) {
@@ -95,12 +113,37 @@ public class ClonePrinter extends AbstractElementPrinter {
 						}
 					}
 
-					pw.println("for (", elementType, " _element: " + MethodHelper.toGetter(context.getDtoFieldName()) + " {");
-					pw.println(newFieldName + "." + MethodHelper.toSetter(context.getDomainFieldName()) + "(_element);");
+					boolean dtoHasClone = false;
+
+					DomainType domain = processingEnv.getTransferObjectUtils().getDtoType(elementType).getDomain();
+					
+					if (domain != null && domain.getDomainDefinitionConfiguration() != null) {
+						dtoHasClone = new GenerateCloneAccessor(domain.getDomainDefinitionConfiguration().asConfigurationElement(), processingEnv).generate();
+					}
+					
+					pw.println("for (", elementType, " _element: " + MethodHelper.toGetter(context.getDtoFieldName()) + ") {");
+					pw.print(newFieldName + ".add(_element");
+					if (dtoHasClone) {
+						pw.print(".clone()");
+					}
+					pw.println(");");
 					pw.println("}");
+					pw.println(RESULT_NAME + "." + MethodHelper.toSetter(context.getDtoFieldName()) + "(" + newFieldName + ");");
 					pw.println("}");
 				} else {
-					pw.println(RESULT_NAME + "." + MethodHelper.toSetter(context.getDtoFieldName()) + "(" + MethodHelper.toGetter(context.getDtoFieldName()) + ");");
+					
+					boolean dtoHasClone = false;
+					if (context.getDomainMethodReturnType().getDomainDefinitionConfiguration() != null) {
+						dtoHasClone = new GenerateCloneAccessor(context.getDomainMethodReturnType().getDomainDefinitionConfiguration().asConfigurationElement(), processingEnv).generate();
+					}
+					
+					pw.print(RESULT_NAME + "." + MethodHelper.toSetter(context.getDtoFieldName()) + "(" + MethodHelper.toGetter(context.getDtoFieldName()));
+					
+					if (dtoHasClone) {
+						pw.print(".clone()");
+					}
+					
+					pw.println(");");
 				}
 				return;
 			case ARRAY:
